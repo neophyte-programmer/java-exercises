ab57d2ea422d795c799ee337170b3b955671499c
Nutifafa Attor <attorfafa@gmail.com>
2022-07-06 07:37:57 +0000

    loop through multiplication

--- times-table/src/com/company/Main.java
+++ times-table/src/com/company/Main.java
@@ -1,10 +1,11 @@
 package com.company;
+
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// Write a Java program that takes a number as input and prints its multiplication table up to 10.
+        // Write a Java program that takes a number as input and prints its multiplication table up to 10.
 
         // Read user input
         Scanner sc = new Scanner(System.in);
