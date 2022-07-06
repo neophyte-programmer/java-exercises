a0d8b57b1dd196dc62e8605b7970ed1972edb282
Nutifafa Attor <attorfafa@gmail.com>
2022-07-06 09:49:32 +0000

    skeleton for methods

---
+++ area-of-objects/src/com/company/Main.java
@@ -0,0 +1,81 @@
+package com.company;
+
+public class Main {
+//    2 DIMENSIONAL OBJECTS
+    // Area of circle
+    public static float circle() {
+        return 5;
+    }
+
+    // Area of triangle
+    public static float triangle() {
+        return 5;
+    }
+
+    // Area of square
+    public static float square() {
+        return 5;
+    }
+
+    // Area of rectangle
+    public static float rectangle() {
+        return 5;
+    }
+
+    // Area of parallelogram
+    public static float parallelogram() {
+        return 5;
+    }
+
+    // Area of trapezium
+    public static float trapezium() {
+        return 5;
+    }
+
+    // Area of ellipse
+    public static float ellipse() {
+        return 5;
+    }
+
+    // Area of polygon
+    public static float polygon() {
+        return 5;
+    }
+
+//    3 DIMENSIONAL OBJECTS
+    // Area of cube
+    public static float cube() {
+        return 5;
+    }
+
+    // Area of rectangular prism
+    public static float rectangularPrism() {
+        return 5;
+    }
+
+    // Area of cylinder
+    public static float cylinder() {
+        return 5;
+    }
+
+    // Area of cone
+    public static float cone() {
+        return 5;
+    }
+
+    // Area of sphere
+    public static float sphere() {
+        return 5;
+    }
+
+    // Area of hemisphere
+    public static float hemisphere() {
+        return 5;
+    }
+
+    public static void main(String[] args) {
+	// Calculate the area of objects
+//  2D - circle, triangle, square, rectangle, parallelogram, trapezium, ellipse, polygon
+//  3D - cube, rectangular prism, cylinder, cone, sphere, hemisphere,
+    }
+}
