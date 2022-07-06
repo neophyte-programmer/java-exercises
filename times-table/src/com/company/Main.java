2b04d70a891000662bff19e2812275d5afee4e98
Nutifafa Attor <attorfafa@gmail.com>
2022-07-06 07:32:09 +0000

    read user input

---
+++ times-table/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Write a Java program that takes a number as input and prints its multiplication table up to 10.
+
+        // Read user input
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your times table number: ");
+        int number = sc.nextInt();
+
+        // Loop through multiplication
+        for (int i = 1; i <= 12; i++) {
+            System.out.println(i);
+        }
+
+
+    }
+}
