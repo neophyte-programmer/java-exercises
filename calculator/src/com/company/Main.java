6a8d48669fc135f10e38f27101579a23c606ae82
Nutifafa Attor <attorfafa@gmail.com>
2022-07-06 06:00:02 +0000

    wrote comments

---
+++ calculator/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    //	Write a Java program to print the sum, product, difference, quotient and remainder of two numbers.
+
+        // Take input
+
+        // Perform addition
+
+        // Perform subtraction
+
+        // Perform multiplication
+
+        // Perform division
+
+        // Perform modulo
+
+        // Print results
+    }
+}
